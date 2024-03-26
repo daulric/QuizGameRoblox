646f015b6a54e5104f3a41b7dc4bb0950c9134fc
Ulric <ulricaird1@gmail.com>
2024-03-26 16:52:23 -0400

    Preparing For the Next Update
    This allows the user to choose their own category to do the quiz

--- Game/Shared/Server/Controllers/QuizApi.lua
+++ Game/Shared/Server/Controllers/QuizApi.lua
@@ -9,7 +9,8 @@ local rednet = exon.rednet
 local QuizApi = Controllers.CreateController {
     Name = "Quiz Api",
     Info = {
-        QuizInfo = {}
+        QuizInfo = {},
+        Categories = {},
     },
 
     Signal = rednet.createSignal(),
@@ -22,9 +23,41 @@ function QuizApi:Fire(data)
     self.Signal:Fire(data)
 end
 
+function QuizApi:GetCategories()
+    local url = "https://opentdb.com/api_category.php"
+    local urlData = HttpService:GetAsync(url)
+    local info = HttpService:JSONDecode(urlData)
+
+    for i, v in pairs(info.trivia_categories) do
+        local item = {
+            id = v.id,
+            category = v.name
+        }
+
+        table.insert(self.Info.Categories, item)
+    end
+
+end
+
+function QuizApi:GetCategory(category)
+
+    local chosenId = 0
+
+    for i, v in pairs(self:GetCategories()) do
+        if v.category == category then
+            chosenId = v.id
+            break
+        end
+    end
+
+    return chosenId
+
+end
+
 -- Getting Info using the API
-function QuizApi:Get(amount: number, difficulty: difficulty, type: GameType)
-    local url = `https://opentdb.com/api.php?amount={amount}&category=18&difficulty={difficulty}&type={type}`
+function QuizApi:Get(amount: number, difficulty: difficulty, type: GameType, category)
+    category = category or 18
+    local url = `https://opentdb.com/api.php?amount={amount}&category={category}&difficulty={difficulty}&type={type}`
     local urlData = HttpService:GetAsync(url, true)
     local info = HttpService:JSONDecode(urlData)
 
