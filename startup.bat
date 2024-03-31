05cf3994841329948bd61d7bebe28df0334455c6
Ulric <ulricaird1@gmail.com>
2024-03-31 19:46:57 -0400

    File Shanges

--- startup.bat
+++ startup.bat
@@ -1,2 +1,3 @@
 rojo build -o QuizGame.rbxlx place.project.json
-start rojo serve place.project.json & "C:\Users\ulric\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Roblox\Roblox Studio".lnk -localPlaceFile C:\Users\ulric\OneDrive\Desktop\QuizGameRoblox\QuizGame.rbxlx
+start rojo serve place.project.json
+"C:\Users\ulric\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Roblox\Roblox Studio".lnk -localPlaceFile C:\Users\ulric\OneDrive\Desktop\QuizGameRoblox\QuizGame.rbxlx
